05b5ad4388fa91f9376a4473b4341f7c22ef764f
another-julian <juliancardozo189@outlook.com>
2025-02-17 19:29:25 -0500

    add taks

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,67 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccesToken } from "../libs/jwt.js";
+
+export const register = async (req, res) => {
+  const { email, password, username } = req.body;
+  try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: passwordHash,
+    });
+
+    const userSaved = await newUser.save();
+    const token = await createAccesToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+    res.json({
+      message: "user created",
+      username: userSaved.username,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ message: "user not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect password" });
+
+    const token = await createAccesToken({ id: userFound._id });
+
+    res.cookie("token", token);
+    res.json({
+      message: "Log in succefully",
+      username: userFound.username,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.cookie("token", "", { expires: new Date(0) });
+  return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+
+  if (!userFound) return res.status(400).json({ message: "user not found" });
+
+  return res.json({
+    username: userFound.username,
+    email: userFound.email,
+  });
+};
