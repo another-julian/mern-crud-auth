a4ff779c3f5b7877786dfecf5c371269bf0e60a1
another-julian <juliancardozo189@outlook.com>
2025-02-25 23:01:11 -0500

    making the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,27 @@
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <main className="w-full">
+      <section className="h-screen w-full  bg-light">
+        <div className="h-[80%] flex flex-col justify-end align-middle items-center">
+          <span className="mb-12 text-secondary text-4xl font-medium">
+            welcome to
+          </span>
+          <h1 className="mb-16 text-7xl font-bold text-secondary tracking-wider">
+            <span
+              className="bg-secondary text-light px-7
+          "
+            >
+              TASK
+            </span>{" "}
+            TRACK
+          </h1>
+          <button className="bg-primary py-2 px-10 text-2xl tracking-widest font-bold shadow-lg text-light ">
+            START
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
